2c4c32aa5824e6b99e6a873733b9808ac6c5981b
khaled sabry <134939844+ksabry97@users.noreply.github.com>
2023-07-25 23:16:32 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,51 +1,92 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h> 
+#include <string.h>
 #include "main.h"
+/**
+ * tostring - convert int to string
+ * @str: array
+ * @int: integer
+ */
+
+void tostring(char str[], int num)
+{
+    int i, rem, len = 0, n;
+ 
+    n = num;
+    while (n != 0)
+    {
+        len++;
+        n /= 10;
+    }
+    for (i = 0; i < len; i++)
+    {
+        rem = num % 10;
+        num = num / 10;
+        str[len - (i + 1)] = rem + '0';
+    }
+    str[len] = '\0';
+}
 
 /**
- * _printf - formatted output conversion and print data.
- * @format: input string.
+ * _printf - print string with format
+ * @format: input string
  *
- * Return: number of chars printed.
+ * Return: number of printed characters
  */
+
 int _printf(const char *format, ...)
 {
-	unsigned int i = 0, len = 0, ibuf = 0;
-	va_list arguments;
-	int (*function)(va_list, char *, unsigned int);
-	char *buffer;
-
-	va_start(arguments, format), buffer = malloc(sizeof(char) * 1024);
-	if (!format || !buffer || (format[i] == '%' && !format[i + 1]))
-		return (-1);
-	if (!format[i])
-		return (0);
-	for (i = 0; format && format[i]; i++)
-	{
-		if (format[i] == '%')
-		{
-			if (format[i + 1] == '\0')
-			{	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
-				return (-1);
-			}
-			else
-			{	function = get_print_func(format, i + 1);
-				if (function == NULL)
-				{
-					if (format[i + 1] == ' ' && !format[i + 2])
-						return (-1);
-					handl_buf(buffer, format[i], ibuf), len++, i--;
-				}
-				else
-				{
-					len += function(arguments, buffer, ibuf);
-					i += ev_print_func(format, i + 1);
-				}
-			} i++;
-		}
-		else
-			handl_buf(buffer, format[i], ibuf), len++;
-		for (ibuf = len; ibuf > 1024; ibuf -= 1024)
-			;
-	}
-	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
-	return (len);
+    char *cursor;
+    int count = 0;
+    
+    if (!format)
+    {
+        return 0;
+    }
+    va_list args;
+    va_start(args, format);
+    for (cursor = format; *cursor != '\0'; cursor++)
+    {
+        while (*cursor != '%')
+        {
+            putchar(*cursor);
+            cursor++;
+            count++;
+            if (*cursor == '\0')
+            {
+                return count;
+            }
+        }
+        cursor++;
+        switch(*cursor)
+        {
+            case 'c':
+                putchar(va_arg(args, int));
+                count++;
+                break;
+            case 'd':
+                char str[sizeof(int)];
+                tostring(str, va_arg(args, int));
+                for (int j = 0; j < strlen(str); j++)
+                {
+                    putchar(*(str + j));
+                }
+                count += strlen(str) - 1;
+                break;
+            case 's':
+                char *s;
+                s = va_arg(args, char *);
+                for (int x = 0;  x < strlen(s); x++)
+                {
+                    putchar(*(s + x));
+                }
+                count += strlen(s) - 1;
+                break;
+            default:
+                putchar('%');
+                putchar(*cursor);
+        }
+    }
+    return count;
 }
